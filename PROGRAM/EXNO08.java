ac2a4ee1b870036a0c052453cbf30c018c8cd0ec
prakashmk12 <prakashmk654@gmail.com>
2026-01-27 15:13:55 +0530

    EXNO08.java

--- PROGRAM/EXNO08.java
+++ PROGRAM/EXNO08.java
@@ -1,12 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package PROGRAM;
-/**
- *
- * @author Student
- */
 import java.util.Scanner;
 class Shape {
     double getPerimeter() {
